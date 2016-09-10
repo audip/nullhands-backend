670d5741540eab4de96e6813e7ed78ab5815cfe9
Aditya Purandare <aditya.p1993@hotmail.com>
2016-09-10 12:27:33 -0400

    Add realtime save blink data

--- connect_firebase.py
+++ connect_firebase.py
@@ -1,22 +1,27 @@
 from __future__ import print_function
 from firebase import firebase
-import requests
+import requests, time, json
 
 # Base cURL for reference
 # curl -X POST -d '{"x":"100", "y":"100"}' 'https://camera-db.firebaseio.com/gyro.json?auth=1A1AEwbsKhtw6PJXmsPCAaXsfWt8QolDLDUuXXmy'
 baseURL = 'https://camera-db.firebaseio.com'
 
 def fetch_data(collection):
-    firebase = requests.get('%s/%s.json' % (baseURL, collection))
+    current_limit, i = 500, 0
+    while(i < current_limit):
+        firebase = requests.get('%s/%s.json?limit' % (baseURL, collection))
+        time.sleep(0.5)
     return firebase.json()
 
-def save_data(wink_data):
-    firebase = connect_db()
-    result = firebase.post('wink', wink_data)
+def save_data(collection):
+    wink_data = {'left':'false', 'right':'false'}
+    url = 'https://camera-db.firebaseio.com/wink.json'
+    result = requests.put(url, data=json.dumps(wink_data))
     return result
 
 def main():
-    print(fetch_data('gyro')['x'])
+    # print(fetch_data('gyro')['x'])
+    print(save_data('wink', sample))
 
 
 if __name__ == '__main__':
